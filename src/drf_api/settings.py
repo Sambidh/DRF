0c78735c3b8cb61574b24aadb11a5e2db881dc82
Sambidh <raisambidh@gmail.com>
2024-05-18 19:57:39 +0545

    vid3 complete. dj-rest-auth error solved. maybe needs to be checked again

--- src/drf_api/settings.py
+++ src/drf_api/settings.py
@@ -32,6 +32,10 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
+    'dj_rest_auth',
+
     'core',
     
     'django.contrib.admin',
@@ -124,3 +128,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES' : [
+        'rest_framework.authentication.TokenAuthentication'
+    ]
+}
